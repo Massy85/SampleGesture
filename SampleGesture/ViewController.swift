9e11d8c9940e803513c312bd0e0d67dd2aec8ebb
Massimiliano Bonafede <massimiliano.bonafede@urmet.com>
2021-11-20 15:47:25 +0100

    finisched managed pan reposition and almost finish the pinch reposition

--- SampleGesture/ViewController.swift
+++ SampleGesture/ViewController.swift
@@ -47,14 +47,14 @@ class DragableImageView: UIImageView {
         case .ended:
             lastPointPanned.x = frame.origin.x
             lastPointPanned.y = frame.origin.y
-            //repositionAfterPan()
-            newPanReposition()
+            repositionAfterPan()
         default: break
         }
     }
     
     
     var pinchedView = UIView()
+    var oldPoint: CGPoint = CGPoint(x: 0, y: 0)
     var oldSize: CGSize = CGSize(width: 0, height: 0)
     var size: CGSize = CGSize(width: 0, height: 0)
     
@@ -62,6 +62,7 @@ class DragableImageView: UIImageView {
         switch sender.state {
         case .began:
             oldSize = CGSize(width: frame.width, height: frame.height)
+            oldPoint = CGPoint(x: frame.minX, y: frame.minY)
         case .changed:
             transform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
 
@@ -71,17 +72,7 @@ class DragableImageView: UIImageView {
             lastPointPinched.y = frame.origin.y
             lastPointPanned = lastPointPinched
             size = CGSize(width: frame.width, height: frame.height)
-            if frame.width < bounds.width && size.width < originalSize.width {
-                let scale = self.bounds.width / frame.width
-                //UIView.animate(withDuration: 0.2) {
-                transform = .identity
-                    drawNewFrameForReposition(x: 0, y: 0, width: originalSize.width, height: originalSize.height)
-                    //self.transform = self.transform.scaledBy(x: scale, y: scale)
-                    self.size = self.originalSize
-               // }
-            } else {
-                repositionAfterPinch()
-            }
+            repositionAfterPinch()
         default: break
         }
     }
@@ -93,166 +84,118 @@ class DragableImageView: UIImageView {
     }
     
     private func repositionAfterPinch() {
+        if frame.width < bounds.width && size.width < originalSize.width {
+            transform = .identity
+            drawNewFrameForReposition(x: 0, y: 0, width: originalSize.width, height: originalSize.height)
+            self.size = self.originalSize
+            return
+        }
+        
+//        if transform.isIdentity == false {
+//            transform = .identity
+//            frame = CGRect(x: lastPointPinched.x, y: lastPointPinched.y, width: size.width, height: size.height)
+//            return
+//        }
+        transform = .identity
+        var x: CGFloat = lastPointPinched.x
+        var y: CGFloat = lastPointPinched.y
+        
+        
+        
         if lastPointPinched.x > 0 && lastPointPinched.y > 0 {
-            transform = .identity
-            print("Pinch 1")
-            drawNewFrameForReposition(x: 0, y: 0, width: size.width, height: size.height)
-            return
-        } else if lastPointPinched.x < 0 && lastPointPinched.y > 0 {
-            //transform = .identity
-            print("Pinch 2")
-            let diffX: CGFloat = -(frame.width - size.width)
-            drawNewFrameForReposition(x: lastPointPanned.x - diffX, y: 0, width: oldSize.width, height: oldSize.height)
-        } else {
-            print("Pinch 3")
-        }
-    }
-    
-    func newPanReposition() {
-        if frame.width == bounds.width && size.width == 0 && size.height == 0 {
-            print("0")
-            drawNewFrameForReposition(x: 0, y: 0, width: frame.width, height: frame.height)
-            return
-        }
-    }
-    
-    
-    private func repositionAfterPan() {
-        if frame.width == bounds.width && size.width == 0 && size.height == 0 {
-            print("0")
-            drawNewFrameForReposition(x: 0, y: 0, width: frame.width, height: frame.height)
-            return
-        } else {
-            transform = .identity
-            if frame.minX > 0 && frame.minY > 0 && originalSize.width < size.width {
-                print("1")
-                if lastPointPanned.x < 0 && lastPointPanned.y < 0 {
-                    print("1-1")
-                    frame = CGRect(x: lastPointPanned.x, y: lastPointPanned.y, width: size.width, height: size.height)
-                    return
-                } else {
-                    print("1-2")
-                    drawNewFrameForReposition(x: 0, y: 0, width: size.width, height: size.height)
-                    return
-                }
-            } else if frame.minX > 0 && frame.minY < 0 && frame.maxY < bounds.maxY {
-                print("2-1")
-                let diffY: CGFloat = -(frame.height - originalSize.height)
-                if frame.minX > 0 && frame.minY < diffY {
-                    print("2-2")
-                    let y = -(frame.height - originalSize.height)
-                    drawNewFrameForReposition(x: 0, y: y, width: size.width, height: size.height)
-                    return
-                } else if frame.minX > 0 && frame.minY > diffY {
-                    print("2-3")
-                    drawNewFrameForReposition(x: 0, y: frame.minY, width: size.width, height: size.height)
-                    return
-                } else if frame.minY == -(frame.height - originalSize.height) {
-                    print("2-4")
-                    drawNewFrameForReposition(x: 0, y: frame.minY, width: size.width, height: size.height)
-                    return
-                } else {
-                    print("2-5")
-                    drawNewFrameForReposition(x: frame.minX, y: frame.minY, width: size.width, height: size.height)
-                    return
-                }
-            } else if frame.minX < 0 && frame.minY < 0 && frame.maxX < bounds.maxX && frame.maxY < bounds.height {
-                print("3")
-                print("3-1")
-                let diffX: CGFloat = -(frame.width - originalSize.width)
-                let diffY: CGFloat = -(frame.height - originalSize.height)
-                if frame.minX < diffX && frame.minY < diffY {
-                    print("3-2")
-                    drawNewFrameForReposition(x: diffX, y: diffY, width: size.width, height: size.height)
-                    return
-                } else if frame.minX < diffX && frame.minY < abs(diffY) {
-                    print("3-3")
-                    drawNewFrameForReposition(x: diffX, y: frame.minY, width: size.width, height: size.height)
-                    return
-                } else if frame.minX > diffX && frame.minY < diffY {
-                    print("3-4")
-                    drawNewFrameForReposition(x: frame.minX, y: diffY, width: size.width, height: size.height)
-                    return
-                } else if frame.minX == diffX && frame.minY < diffY {
-                    print("3-5")
-                    drawNewFrameForReposition(x: frame.minX, y: diffY, width: size.width, height: size.height)
-                    return
-                } else {
-                    print("3-6")
-                    self.frame = CGRect(x: lastPointPanned.x, y: lastPointPanned.y, width: size.width, height: size.height)
-                    //drawNewFrameForReposition(x: frame.minX, y: frame.minY, width: size.width, height: size.height)
-                    return
-                }
-                // }
-            } else if frame.minX < 0 && frame.minY > 0 && frame.maxX < bounds.maxX && frame.maxY > bounds.maxY {
-                print("4")
-                if frame.maxX < bounds.maxX && frame.maxY > bounds.maxY {
-                    print("4-1")
-                    let diffX: CGFloat = -(frame.width - originalSize.width)
-                    let diffY: CGFloat = (frame.height - originalSize.height)
-                    if frame.minX < diffX && diffY > 0 {
-                        print("4-2")
-                        drawNewFrameForReposition(x: diffX, y: 0, width: size.width, height: size.height)
-                        return
-                    } else if frame.minX > diffX && diffY > 0 {
-                        print("4-3")
-                        drawNewFrameForReposition(x: frame.minX, y: 0, width: size.width, height: size.height)
-                        return
-                    } else if frame.minX == diffX && frame.minY > 0 {
-                        print("4-4")
-                        drawNewFrameForReposition(x: frame.minX, y: 0, width: size.width, height: size.height)
-                        return
-                    } else if frame.minX < 0 && originalSize == size {
-                        print("4-5")
-                        drawNewFrameForReposition(x: 0, y: 0, width: size.width, height: size.height)
-                        return
-                    } else if lastPointPanned.x < 0 && lastPointPanned.y > 0 {
-                        print("4-6")
-                        drawNewFrameForReposition(x: frame.width - size.width, y: 0, width: size.width, height: size.height)
-                        return
-                    } else {
-                        print("4-7")
-                        drawNewFrameForReposition(x: frame.minX, y: frame.minY, width: size.width, height: size.height)
-                        return
-                    }
-                    
-                } else {
-                    print("4-8")
-                    frame = CGRect(x: frame.minX, y: frame.minY, width: size.width, height: size.height)
-                    return
-                }
+            if size.width < originalSize.width {
+                drawNewFrameForReposition(x: 0, y: 0, width: originalSize.width, height: originalSize.height)
+                self.size = self.originalSize
+                return
             } else {
-                print("5")
-                let diffX = -(frame.width - originalSize.width)
-                let diffY = -(frame.height - originalSize.height)
-                if frame.minX > 0 && frame.minY == 0 {
-                    print("5-1")
-                    drawNewFrameForReposition(x: 0, y: frame.minY, width: size.width, height: size.height)
-                    return
-                } else if frame.minX == 0 && frame.minY > 0 {
-                    print("5-2")
-                    drawNewFrameForReposition(x: frame.minX, y: 0, width: size.width, height: size.height)
-                    return
-                } else if frame.minX < diffX && frame.minY == 0 {
-                    print("5-3")
-                    drawNewFrameForReposition(x: diffX, y: frame.minY, width: size.width, height: size.height)
-                    return
-                } else if frame.minX == 0 && frame.minY < 0 {
-                    print("5-4")
-                    drawNewFrameForReposition(x: frame.minX, y: diffY, width: size.width, height: size.height)
-                    return
-                } else if frame.size == originalSize {
-                    print("5-5")
-                    drawNewFrameForReposition(x: 0, y: 0, width: originalSize.width, height: originalSize.height)
-                } else {
-                    print("5-6")
-                    let x = lastPointPanned.x
-                    let y = lastPointPanned.y
-                    drawNewFrameForReposition(x: x, y: y, width: size.width, height: size.height)
-                    return
-                }
+                drawNewFrameForReposition(x: 0, y: 0, width: oldSize.width, height: oldSize.height)
+                self.size = self.oldSize
+                return
             }
         }
+        
+        if lastPointPinched.x > 0 && lastPointPinched.y < 0 {
+            if size.width < originalSize.width {
+                drawNewFrameForReposition(x: 0, y: 0, width: originalSize.width, height: originalSize.height)
+                self.size = self.originalSize
+                return
+            } else {
+                drawNewFrameForReposition(x: oldPoint.x, y: oldPoint.y, width: oldSize.width, height: oldSize.height)
+                self.size = self.oldSize
+                return
+            }
+        }
+        
+        let heightDiff = -(frame.height - size.height)
+        let widthDiff = -(frame.width - oldSize.width)
+        #warning("Gestire questo caso")
+        if lastPointPinched.x < 0 && lastPointPinched.y < 0 && frame.maxX < originalSize.width {
+            if size.width < originalSize.width {
+                drawNewFrameForReposition(x: 0, y: 0, width: originalSize.width, height: originalSize.height)
+                self.size = self.originalSize
+                return
+            } else {
+                drawNewFrameForReposition(x: oldPoint.x, y: oldPoint.y, width: oldSize.width, height: oldSize.height)
+                self.size = self.oldSize
+                return
+            }
+        }
+        
+        if lastPointPinched.x < 0 && lastPointPinched.y > 0 {
+            if size.width < originalSize.width {
+                drawNewFrameForReposition(x: 0, y: 0, width: originalSize.width, height: originalSize.height)
+                self.size = self.originalSize
+                return
+            } else {
+                drawNewFrameForReposition(x: oldPoint.x, y: oldPoint.y, width: oldSize.width, height: oldSize.height)
+                self.size = self.oldSize
+                return
+            }
+        }
+        
+        frame = CGRect(x: lastPointPinched.x, y: lastPointPinched.y, width: size.width, height: size.height)
+    }
+    
+    func repositionAfterPan() {
+        if frame.width == bounds.width && size.width == 0 && size.height == 0 {
+            print("0")
+            drawNewFrameForReposition(x: 0, y: 0, width: frame.width, height: frame.height)
+            return
+        }
+        
+        if transform.isIdentity == false {
+            transform = .identity
+            frame = CGRect(x: lastPointPanned.x, y: lastPointPanned.y, width: size.width, height: size.height)
+            return
+        }
+        
+        var x: CGFloat = frame.minX
+        var y: CGFloat = frame.minY
+        
+        if frame.minX > 0 {
+            if frame.minX > 0 && frame.maxX > originalSize.width {
+                x = 0
+            } else {
+                let widthDiff = -(frame.width - originalSize.width)
+                x = widthDiff
+            }
+        } else if frame.minX < 0 {
+            let widthDiff = -(frame.width - originalSize.width)
+            if frame.minX < widthDiff {
+                x = widthDiff
+            }
+        }
+        
+        if frame.minY > 0 {
+            y = 0
+        } else if frame.minY < 0 {
+            let heightDiff = -(frame.height - originalSize.height)
+            if frame.minY < heightDiff {
+                y = heightDiff
+            }
+        }
+        
+        drawNewFrameForReposition(x: x, y: y, width: size.width, height: size.height)
     }
 }
 
